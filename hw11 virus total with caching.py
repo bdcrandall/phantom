22748f9986a66cbafaf1b2d3e09b566949f3c4cc
admin <root@localhost>
2022-03-06 14:42:07 +0000

    Saved: debugging

--- hw11 virus total with caching.py
+++ hw11 virus total with caching.py
@@ -204,7 +204,7 @@ def update_cache(action=None, success=None, container=None, results=None, handle
     # Use variable names that are easier to follow
     cacheOperation = check_cache__cacheOperation
     cacheIndex = check_cache__cacheIndex
-    fileHash = filtered_artifacts_item_1_0
+    fileHash = filtered_artifacts_item_1_0[0]
     fileName = results_item_1_1[0]
     fileReputation = results_item_1_0[0]
     fileLastAnalyzed = results_item_1_2[0]
